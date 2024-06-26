aa6486854eab9145b235bcc592896e62f8d9a90a
João Pedro Gonçalves Viana de Souza <107888675+JoaoVianaSouza@users.noreply.github.com>
2024-06-26 16:54:56 -0300

    Delete API-CEP/ApiBuscaCep directory

--- API-CEP/ApiBuscaCep/src/main/java/io/github/JoaoVianaSouza/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.JoaoVianaSouza.repository;
-
-import io.github.JoaoVianaSouza.models.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
